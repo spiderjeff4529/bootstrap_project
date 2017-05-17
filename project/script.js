c5ef96414994adf678419a5bd3f41e97d6d73227
luisc8487 <luisc8487@hstat.org>
2017-05-17 02:00:27 +0000

    slides

--- project/script.js
+++ project/script.js
@@ -9,3 +9,14 @@ function collapseNavbar() {
 }
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
+
+$(window).scroll(function() {
+  $(".info").each(function() {
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
